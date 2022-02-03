bea7ae1499dc142fe05e898b83952e362bfd5f7d
vivek soni <sonivivek346@gmail.com>
2022-02-03 16:34:16 +0530

    Day-1 program added

---
+++ DAY-1/src/com/vivek/Example.java
@@ -0,0 +1,20 @@
+package com.vivek;
+
+import java.sql.SQLOutput;
+
+public class Example {
+
+    public static void main(String args[]) {
+        // declaring character
+        char a = 'G';
+
+        // Integer data type is generally
+        // used for numeric values
+        int i = 89;
+
+        // use byte and short
+        // if memory is a constraint
+        System.out.println(a);
+        System.out.println(i);
+    }
+}
---
+++ DAY-1/src/com/vivek/Test2.java
@@ -0,0 +1,17 @@
+package com.vivek;
+
+public class Test2
+{
+
+        public static void main(String args[])
+        {
+            int x;
+            for (x = 0; x < 4; x++)
+            {
+                System.out.println(x);
+            }
+
+            System.out.println(x);
+        }
+    }
+
